f90166f3d3007fc443fa412ae7404e1e89bcd0d3
tinytransfem <n3rilp@gmail.com>
2024-04-18 17:40:49 +0200

    fixed key mapping not being configurable

--- src/main/java/org/infernalstudios/betterbridging/client/KeyMappings.java
+++ src/main/java/org/infernalstudios/betterbridging/client/KeyMappings.java
@@ -2,12 +2,21 @@ package org.infernalstudios.betterbridging.client;
 
 import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.client.settings.KeyConflictContext;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import org.infernalstudios.betterbridging.BetterBridging;
 import org.lwjgl.glfw.GLFW;
 
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class KeyMappings {
     public static final String KEY_CYCLE = "key." + BetterBridging.MOD_ID + ".cycle";
 
     public static final KeyMapping CYCLE_KEY = new KeyMapping(KEY_CYCLE, KeyConflictContext.IN_GAME, InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_R, KeyMapping.CATEGORY_GAMEPLAY);
+
+    @SubscribeEvent
+    public static void onKeyRegister(RegisterKeyMappingsEvent event) {
+        event.register(KeyMappings.CYCLE_KEY);
+    }
 }
