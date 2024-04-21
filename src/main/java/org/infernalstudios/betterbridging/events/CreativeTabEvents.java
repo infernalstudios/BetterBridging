e484f2836d43e7b8e05f472a6bb675ca697d68fd
tinytransfem <n3rilp@gmail.com>
2024-04-21 18:37:00 +0200

    Initial 1.19.4 port

--- src/main/java/org/infernalstudios/betterbridging/events/CreativeTabEvents.java
+++ src/main/java/org/infernalstudios/betterbridging/events/CreativeTabEvents.java
@@ -1,7 +1,7 @@
 package org.infernalstudios.betterbridging.events;
 
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.infernalstudios.betterbridging.items.ItemsInit;
@@ -9,8 +9,8 @@ import org.infernalstudios.betterbridging.items.ItemsInit;
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class CreativeTabEvents {
     @SubscribeEvent
-    public static void registerCreativeTab(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
+    public static void registerCreativeTab(CreativeModeTabEvent.BuildContents event) {
+        if (event.getTab() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
             event.accept(ItemsInit.BRIDGE_BRACE);
         }
     }
